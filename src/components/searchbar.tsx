9529e8b8e3ad23d262888810994cd8137e9c6d03
Zilan Ye <zilan.daisyye@gmail.com>
2024-12-23 01:31:40 -0500

    searchbar

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -2,15 +2,19 @@ import React, {useState} from 'react'
 
 const SearchBar = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [displayText, setDisplayText] = useState("");
 
   const handleChange = (e: any) => {
     e.preventDefault();
     setSearchInput(e.target.value);
   };
 
-  if (searchInput.length > 0) {
-    return searchInput;
-  }
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setDisplayText(searchInput);
+      setSearchInput(""); // clear input after enter
+    }
+  };
 
   return (
     <div>
@@ -18,8 +22,13 @@ const SearchBar = () => {
         type="search"
         placeholder="Search here"
         onChange={handleChange}
+        onKeyDown={handleKeyPress}
         value={searchInput}
       />
+      <div>
+        <p>{displayText}</p> 
+        {/* display input */}
+      </div>
       <table>
         <thead>
           <tr>
