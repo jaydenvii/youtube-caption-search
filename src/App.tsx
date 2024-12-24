cef4f2347d9b83ed98756e98612d24d07724e347
jaydenvii <jaydenyz456@gmail.com>
2024-12-24 15:23:58 -0500

    Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import SearchBar from './components/searchbar'
-import YouTubeVideoList from "./components/getContent"
-import Card from "./components/Card.tsx"
+import SearchBar from "./components/searchbar";
+import YouTubeVideoList from "./components/getContent";
+import Card from "./components/Card.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-       <Card
+      <Card
         title="hi"
         description="yes."
         // imageUrl="https://please_help_im_dying_ty"
       >
-        <SearchBar/>
+        <SearchBar />
       </Card>
       <h1>Video List</h1>
       <YouTubeVideoList />
-
     </div>
-)
+  );
 }
 
-export default App
+export default App;
