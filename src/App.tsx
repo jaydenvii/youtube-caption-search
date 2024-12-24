129e8eeea2fd8ec5e0dc6c5bbccf85dad937e17f
Zilan Ye <zilan.daisyye@gmail.com>
2024-12-24 01:13:53 -0500

    Revert "ok"
    This reverts commit 6f219881d73315db195633247ef601eea717df2e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
-import "./App.css";
-import SearchBar from "./components/searchbar";
-import YouTubeVideoList from "./components/getContent";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SearchBar from './components/searchbar'
 
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className='App'>
       <SearchBar />
-      <h1>Video List</h1>
-      <YouTubeVideoList />
     </div>
-  );
+)
 }
 
-export default App;
+export default App
