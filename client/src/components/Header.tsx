7ef6cc30b0ab9c675975cf1076cf999291182730
jaydenvii <jaydenyz456@gmail.com>
2025-01-03 17:28:17 -0500

    Clicking on logo sends you back to home page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <h1 className="text-6xl text-center text-blue-700 pt-3">
+      <h1
+        className="text-6xl text-center text-blue-700 pt-3 hover:cursor-pointer"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
         YouTube Caption Search
       </h1>
       <h2 className="text-2xl text-center text-blue-500 pt-3 pb-10">
